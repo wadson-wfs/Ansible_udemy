22596d956ae87de78c11d33d9d32c7c8c763d6c1
spr00497 <wadson.santos@spread.com.br>
2022-05-01 10:13:19 -0300

    teste

--- centos8/provision.sh
+++ centos8/provision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo su
-#dnf -y install epel-release
+dnf -y install epel-release
 #echo "inicio da instalação do ansible"
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-sudo yum update -y
-yum install -y vim net-tools
+#cd /etc/yum.repos.d/
+#sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+#sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+dnf update -y
+dnf install -y vim net-tools
 #cat <<EOT >> /etc/hosts
 #192.168.15.99 ansible
 #192.168.1.3 app01
